8d91e226385e2020d3fe807784e9bb91301a237a
joao tiago viegas <joao.viegas@maersk.com>
2020-08-22 00:21:32 +0200

    added function to update bashutils.inc

--- helper.sh
+++ helper.sh
@@ -9,16 +9,25 @@ if [ -z "$this_folder" ]; then
 fi
 parent_folder=$(dirname "$this_folder")
 
-# --- START include bashutils SECTION ---
-_pwd=$(pwd)
-cd "$this_folder"
-url=$(curl -s https://api.github.com/repos/tgedr/bashutils/releases/latest | grep "browser_download_url.*utils\.tar\.bz2" | cut -d '"' -f 4)
-curl "$url" -o utils.tar.bz2
-tar xjpvf utils.tar.bz2
-rm utils.tar.bz2
 . "$this_folder/bashutils.inc"
-cd "$_pwd"
-# --- END include bashutils SECTION ---
+
+
+update_bashutils(){
+  echo "[update_bashutils] ..."
+
+  _pwd=`pwd`
+  cd "$this_folder"
+
+  curl -s https://api.github.com/repos/tgedr/bashutils/releases/latest \
+  | grep "browser_download_url.*utils\.tar\.bz2" \
+  | cut -d '"' -f 4 | wget -qi -
+  tar xjpvf utils.tar.bz2
+  if [ ! "$?" -eq "0" ] ; then echo "[update_bashutils] could not untar it" && cd "$_pwd" && return 1; fi
+  rm utils.tar.bz2
+
+  cd "$_pwd"
+  echo "[update_bashutils] ...done."
+}
 
 debug "1: $1 2: $2 3: $3 4: $4 5: $5 6: $6 7: $7 8: $8 9: $9"
 
@@ -26,6 +35,9 @@ debug "1: $1 2: $2 3: $3 4: $4 5: $5 6: $6 7: $7 8: $8 9: $9"
 case "$1" in
   zip_js_lambda_function)
     zip_js_lambda_function "${@:2}"
+    ;;
+  update_bashutils)
+    update_bashutils
     ;;
   *)
     usage
